df807c9fca977272fb1acbe42f3821438d0f898a
Ritwik Sudhakar Tat <ritwikt17@gmail.com>
2026-04-27 14:43:11 +0530

    update server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config();
 const path = require('path');
@@ -7,6 +8,7 @@ const { processJobApplication } = require('./src/agent');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'frontend')));
